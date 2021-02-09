b4856e033c0c2d22efe2f230cca57c8efaa2b5a3
Ettore Rossatto <ettore.rossatto@ciadaconsulta.com.br>
2021-02-08 21:43:45 -0300

    Create menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderModule } from './header/header.module';
+import { MenuModule } from './menu/menu.module';
 
 import { AppComponent } from './app.component';
 
@@ -18,6 +17,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     CommonModule,
     HeaderModule,
+    MenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
